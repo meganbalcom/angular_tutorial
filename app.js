9fd9b9ae6628757824f670a317b4d08500d90307
Megan Balcom <meg.balcom@gmail.com>
2016-02-08 21:26:51 -0500

    Modified app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 (function() {
   var app = angular.module('gemStore', ['store-directives']);
 
-app.controller('StoreController', ['$http', function($http){
-  var store = this;
+app.controller('StoreController', function(){
   store.products = gems;
-}]);
-                                
-
+});
+   
   app.controller('ReviewController', function() {
     this.review = {};
 
